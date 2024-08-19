bbaffec2925a1bebd9103166716c08d2e0e0024e
Beyzagrbz <77171797+Beyzagrbz@users.noreply.github.com>
2024-08-19 14:28:14 +0300

    New Commit

---
+++ Collections.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+//koleksiyonlar diziler
+var firstCollect = ["Atıl","Levent","Beyza"]
+firstCollect[2].uppercased()
+firstCollect[0]="Uzay"
+print (firstCollect)
+var secondCollect=[10,4,8,6]
+secondCollect[2]*8/10
+var mixCollect = [true,"Beyza","Levent"] as [Any]
+mixCollect[2]
+mixCollect.append("atıl")
+secondCollect.sort()
+print(secondCollect)//siralanmıs sekilde attı.
---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var userName = "beyza"
+print (userName)
+//let değişmeyen bir sabiti tanımlar..
+let pi=3.14 //double
+pi*5
+userName.uppercased()
+pi.bitPattern.isMultiple(of: 2);
+//pi 2 nin çarpımı mı onu kontrol eder
+var benimbooleanım=true;
+if benimbooleanım==true{
+    print ("happy hours")
+}
+
+
+
+
+
+
+
+
